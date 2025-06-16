38ec241f7f5195188bada56f4fa67fdf53010da3
c0dy1o <68795433+c0dy1o@users.noreply.github.com>
2025-06-15 21:03:57 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+def main():
+    # Main function
+    print("Starting Main Function")
+    initial_position = np.array([7000,0,0])
+
+    initial_velocity = np.array([0, 7.72, 5])
+    integration_time = 24*60*60
+    integration_steps = 1000                                                                   
+
+    trajectory, times = keplerian_propagator(initial_position, initial_velocity, integration_time, integration_steps)
+
+    # Plot it
+    fig = plt.figure()
+    # Define axes in that figure
+    ax = plt.axes(projection='3d',computed_zorder=False)
+    # Plot x, y, z
+    ax.plot(trajectory[0],trajectory[1],trajectory[2],zorder=5)
+    plt.title("All Orbits")
+    ax.set_xlabel("X-axis (km)")
+    ax.set_ylabel("Y-axis (km)")
+    ax.set_zlabel("Z-axis (km)")
+    ax.xaxis.set_tick_params(labelsize=7)
+    ax.yaxis.set_tick_params(labelsize=7)
+    ax.zaxis.set_tick_params(labelsize=7)
+    ax.set_aspect('equal', adjustable='box')
+    plt.show()
+
+def keplerian_propagator(init_r, init_v, tof, steps):
+    """
+    Function to propagate a given orbit
+    """
+    # Time vector
+    tspan = [0, tof]
+    # Array of time values
+    tof_array = np.linspace(0,tof, num=steps)
+    init_state = np.concatenate((init_r,init_v))
+    # Do the integration
+    sol = solve_ivp(fun = lambda t,x:keplerian_eoms(t,x), t_span=tspan, y0=init_state, method="DOP853", t_eval=tof_array, rtol = 1e-12, atol = 1e-12)
+
+    # Return everything
+    return sol.y, sol.t
+
+
+def keplerian_eoms(t, state):
+    """
+    Equation of motion for 2body orbits
+    """
+    # Gravitational Constant times Earth mass
+    earth_mu = 398600.441500000
+    # Extract values from init
+    x, y, z, vx, vy, vz = state
+    r_dot = np.array([vx, vy, vz])
+    
+    # Define r
+    r = np.linalg.norm([x, y, z])
+    # Solve for the acceleration
+    ax = - (earth_mu/r**3) * x
+    ay = - (earth_mu/r**3) * y
+    az = - (earth_mu/r**3) * z
+
+    v_dot = np.array([ax, ay, az])
+
+    dx = np.append(r_dot, v_dot)
+
+    return dx
+
+
+if __name__ == '__main__':
+    main()
