e993f20c977fd131072e0854e2edad8ce2dd257c
thx1138bby <128946660+thx1138bby@users.noreply.github.com>
2025-06-25 10:42:04 -0700

    Update solarsail.py

--- solarsail.py
+++ solarsail.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 import math
 
-arrived = False
-
 def main():
     """
     Main function
@@ -20,22 +18,19 @@ def main():
     marsRad = 228e6
     marsVel = (sun_mu/marsRad)**0.5
     
-    earthInitPos = np.array([earthRad,0,0])
-    earthInitVel = np.array([0,earthVel, 0])
-    marsInitPos = np.array([0, marsRad, 0])
-    marsInitVel = np.array([-marsVel, 0, 0])
+    earthInitPos = np.array([earthRad, 0, 0])
+    earthInitVel = np.array([0, earthVel, 0])
+    marsInitPos = np.array([marsRad, 0, 0])
+    marsInitVel = np.array([0, marsVel, 0])
 
-    integration_time = 24*60*60*28*6.54
+    integration_time = 24*60*60*365*3 # three years, arbitrarily high to ensure no cutoff
     integration_steps = 1000
 
-    # Delta V of ship (Hohmann)
-    shipDeltaV1 = ((sun_mu/earthRad)**0.5) * ((2*marsRad/(earthRad+marsRad))**0.5 - 1) # delta v from departing burn (km/s)
-    shipDeltaV2 = ((sun_mu/marsRad)**0.5) * (1 - (2*earthRad/(earthRad+marsRad))**0.5) # delta v from arriving burn (km/s)
-    shipInitVel = [0, earthVel+shipDeltaV1, 0]
+    shipInitVel = [0, earthVel, 0]
 
     earth, times = keplerian_propagator(earthInitPos, earthInitVel, integration_time, integration_steps)
     mars, times = keplerian_propagator(marsInitPos, marsInitVel, integration_time, integration_steps)
-    ship, times = ship_propagator(earthInitPos, shipInitVel, integration_time, integration_steps)
+    ship, times, arrival_time = ship_propagator(earthInitPos, shipInitVel, integration_time, integration_steps)                                   
     # Plot it
     fig = plt.figure()
     # Define axes in that figure
@@ -53,13 +48,18 @@ def main():
     ax.zaxis.set_tick_params(labelsize=7)
     ax.set_aspect('equal', adjustable='box')
 
-    if arrived:
-        print(arrived)
-
-    print(integration_time/86400)
+    if arrival_time is not None:
+        print(arrival_time/86400)
 
     plt.show()
-    
+
+def reach_mars_event(t, state):
+    x, y, z = state[:3]
+    r = np.linalg.norm([x, y, z])
+    return r - 228e6
+
+reach_mars_event.terminal = True
+reach_mars_event.direction = 1 
 
 def keplerian_propagator(init_r, init_v, tof, steps):
     """
@@ -86,11 +86,12 @@ def ship_propagator(init_r, init_v, tof, steps):
     tof_array = np.linspace(0,tof, num=steps)
     init_state = np.concatenate((init_r,init_v))
     # Do the integration
-    sol = solve_ivp(fun = lambda t,x:ship_eoms(t,x), t_span=tspan, y0=init_state, method="DOP853", t_eval=tof_array, rtol = 1e-12, atol = 1e-12)
+    sol = solve_ivp(fun = lambda t,x:ship_eoms(t,x), t_span=tspan, y0=init_state, method="DOP853", t_eval=tof_array, rtol = 1e-12, atol = 1e-12, events=reach_mars_event)
+
+    mars_arrival_time = sol.t_events[0][0] if sol.t_events[0].size > 0 else None
 
     # Return everything
-    return sol.y, sol.t
-
+    return sol.y, sol.t, mars_arrival_time
 
 def keplerian_eoms(t, state):
     """
@@ -131,7 +132,7 @@ def ship_eoms(t, state):
     solar_constant = 1.361e6 # kw/m^2 * 1e6 m^2/km^2
     au = 150e6 # km
     lightspeed = 299792 # km/s
-    sail_width = 2 # km
+    sail_width = 5 # km
     sail_area = sail_width**2 # km^2
     rad_pressure = solar_constant/(lightspeed*(r/au)**2) # Pa
     force = sail_area*rad_pressure
@@ -147,10 +148,6 @@ def ship_eoms(t, state):
     v_dot = np.array([ax, ay, az])
 
     dx = np.append(r_dot, v_dot)
-
-    global arrived
-    if r >= 228e6:
-        arrived = True
 
     return dx
 
